aab93c9fc7317d4b9c30db504c8fa4ff6c916ef2
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-09-06 10:46:09 +0530

    Create demoonjdbc1.java

---
+++ demoonjdbc1.java
@@ -0,0 +1,36 @@
+/* This is java program in which we retrive the data from the database 
+ * By Prashant Bansal
+ *
+ package com.slot1jdbc;
+
+
+import java.sql.*;
+public class demoonjdbc1 {
+    public static void main(String[] args) throws Exception {
+        //TODO Auto-generated method stub
+        //loading and register
+        Class.forName("com.msql.cj.jdbc.Driver");
+        String url="jdbc:mysql://localhost:3306/anudipdeeptech";
+        String username="root";
+        String password="root";
+        String query = "select * from anudipdeeptech ";
+
+
+        //getting connection
+        Connection con=DriverManager.getConnection(url,username,password);
+        //creation of statement
+        Statement st= con.createStatement();
+        //execute statement
+        ResultSet rs = st.executeQuery(query);
+        rs.next();
+        //go to next element
+        //process the result
+        String emp ="Employee name is "+rs.getString("ename")+" and his/her working in"+rs.getString("edomain")+"with package"+rs.getString("esalary");
+        System.out.println(emp);
+        //close connection
+        con.close();
+    }
+}
+
+ 
+ 
