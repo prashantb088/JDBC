6aa836466aafbfc1e0b796b1d4bb5ba88644688e
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-09-06 22:26:08 +0530

    Update emp.java

--- emp.java
+++ emp.java
@@ -1,6 +1,12 @@
-package com.slot2jdbc;
+/** Illustrating to ADD DATA TO DATABASE AND PRINT DATA OF ONE OR ALL EMPLOYEE USING DATA ACCESS OBJECT
+ *  By Prashant Bansal
+ *
+ */
 
 //model
+package com.slot2jdbc;
+
+
 
 public class emp {
   int eid;
