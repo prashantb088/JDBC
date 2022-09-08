e7f521d0fbdaa9d068019931b53de9b6a1b8894b
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-09-08 17:59:58 +0530

    Update bankingmain.java

--- bankingmain.java
+++ bankingmain.java
@@ -4,6 +4,7 @@
  * By Prashant Bansal
  */
 
+
 package com.slot2jdbc;
 import java.util.*;
 
@@ -12,27 +13,28 @@ public class bankingmain {
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
+		
 		Scanner bs = new Scanner(System.in);
 		bankingdao dao = new bankingdao();
 		bankingCustomer b1 = new bankingCustomer();
 		
 		System.out.println("\t\t-----Welcome to Indian Bank-----");
 		
-		System.out.println("Select Operation : \n1 for Register Account\n2 for Login");
+		System.out.println("Select Operation : \n1 Press 1 For Register Account\n2 Press 2 For Login");
 		int op = bs.nextInt();
 		
 		switch(op) {
 		
 		case 1->{
 			//Creating New Account
-			System.out.println("Enter Customer Details to create new Account");
-			System.out.print("Enter Customer Name");
+			System.out.println("Enter Customer Details to create new Account : ");
+			System.out.print("Enter Customer Name : ");
 			String cname = bs.next();
-			System.out.print("Create Customer Password");
+			System.out.print("Create Customer Password : ");
 			String cpwd = bs.next();
-			System.out.print("Enter Customer Phone");
+			System.out.print("Enter Customer Phone : ");
 			String cphone = bs.next();
-			System.out.print("Enter Acc balance");
+			System.out.print("Enter Acc balance : ");
 			int caccbal = bs.nextInt();
 			b1.cname = cname;
 			b1.cpassword = cpwd;
@@ -41,11 +43,11 @@ public class bankingmain {
 			
 			dao.connect();
 			int res=dao.registerCustomer(b1);
-			if(res!=0) {
-				System.out.println("Account Created");
+			if(res>0) {
+				System.out.println("\t\t--Account Created--");
 			}
 			else {
-				System.out.println("Something went wrong,Please tryagain");
+				System.out.println("User already existed in System");
 				
 			}
 			
@@ -62,7 +64,7 @@ public class bankingmain {
 			dao.connect();
 			int res = dao.login(cname , cpwd);
 			if(res==0) {
-				System.out.println("Wrong Username or password!!!");
+				System.out.println("--Ooops!!--Wrong Username or password!!!--");
 				
 			}
 			else if(res==-1) {
@@ -73,32 +75,79 @@ public class bankingmain {
 				System.out.println("Login success!!!");
 				
 				//We are giving access to Withdraw or Deposit
-				System.out.println("Select Operation :\n1 for Withdraw \n2 for deposit");
-				int op2 = bs.nextInt();
-				switch(op2) {
+				int op2=0;
+				while(op2<5) {
+				        System.out.println("Select Operation :\n1 For Withdraw \n2 For deposit \n3 For check balance \n4 For pin change \n5 For Exit");
+				        op2 = bs.nextInt();
+				        
+				        switch(op2) {
 				
 				case 1->{
-					//Withdraw Amount
+					
+					//Withdraw Amount Method
 					System.out.println("Enter amount to withdraw : ");
 					int amt = bs.nextInt();
 					int res2 = dao.withdraw(res,amt);
 					if(res2<=0) {
+						System.out.println("--Something went wrong--");
+						
+					}
+					else {
+						System.out.println("--Success!!--Withdraw done, updated balance "+res2);
+					}
+					
+			        }
+				case 2->{
+					 
+				       //Depositing Method
+					System.out.println("Enter Amount to Deposit : ");
+					int amount = bs.nextInt();
+					int res2 = dao.deposit(res , amount);
+					if(res2 ==-1) {
 						System.out.println("Something went wrong");
 						
 					}
 					else {
-						System.out.println("Withdraw done");
+						System.out.println("Deposit Done Updated Balance is " +res2);
 					}
+				}
+				
+				case 3->{
 					
+				       //Balance Checking Method
+					System.out.println("Your Balance is +"+dao.checkBalance(res));
+					
+				}
+				
+				case 4->{
+					
+				    //Pin Change Method
+					System.out.println("Enter Your password");
+					String pwd = bs.next();
+					System.out.println("Enter New Password");
+					String newPin = bs.next();
+					int count = dao.pinChange(res , pwd,newPin);
+					
+					if(count<=0) {
+						System.out.println("--Sorry!!--Password Not Matched--");
+						
+					}
+					else {
+						System.out.println("Password Updated");
+					}
 				 }
 				 }
 				
 			}
 			}
 		    }
+		    }
 		
-		bs.close();
-		}
+		    //Closing the Scanner 
+		    bs.close();
+		    }
 		
 }
+
+
 
